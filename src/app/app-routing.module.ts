b0781b055e55bf493b0a28e921ba40fd37f29306
Brando Jesus <brandojesus011@gmail.com>
2019-05-13 11:14:39 -0500

    agregando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { DetailBookComponent } from './components/detail-book/detail-book.component';
+import { ListBooksComponent } from './components/admin/list-books/list-books.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'offers', component: OffersComponent }, //TODO: only user auth
+  { path: 'book/:id', component: DetailBookComponent },
+  { path: 'admin/list-books', component: ListBooksComponent }, //TODO: only user auth
+  { path: 'user/login', component: LoginComponent },
+  { path: 'user/profile', component: ProfileComponent }, //TODO: only user auth
+  { path: 'user/register', component: RegisterComponent },
+  { path: '**', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
